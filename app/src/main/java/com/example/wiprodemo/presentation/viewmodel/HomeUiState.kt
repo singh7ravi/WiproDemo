1ba902ed412cec46597d58f9d32fb68e957c625f
ravi singh <ravindrasinghsolanki41@gmail.com>
2026-07-20 06:25:02 +0530

    placed local user_images

--- app/src/main/java/com/example/wiprodemo/presentation/viewmodel/HomeUiState.kt
+++ app/src/main/java/com/example/wiprodemo/presentation/viewmodel/HomeUiState.kt
@@ -3,12 +3,7 @@ package com.example.wiprodemo.presentation.viewmodel
 import com.example.wiprodemo.data.model.UserDto
 import com.example.wiprodemo.domain.model.User
 
-/*
-sealed class HomeUiState {
-    data object Loading : HomeUiState()
-    data class Success(val user: User): HomeUiState()
-    data class Error(val errorMsg: String): HomeUiState()
-}*/
+
 
 
 sealed class HomeUiState {
